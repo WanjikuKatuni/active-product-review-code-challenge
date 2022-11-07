5a11bdceab331285f539c6fe47444aaa95409907
wanjiku <annritawanjiku77@gmail.com>
2022-11-07 10:39:12 +0300

    setup reviews table

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,4 +23,21 @@ puts "Creating reviews..."
 # *****************************************************************
 # Create Reviews Here
 
+Review.create(star_rating: 5, comment: 'Amazing', product_id: p1.id, user_id:user1.id)
+Review.create(star_rating: 5, comment: 'Awesome', product_id: p4.id, user_id:user2.id)
+Review.create(star_rating: 4, comment: 'Great', product_id: p5.id, user_id:user1.id)
+Review.create(star_rating: 1, comment: 'Could be better', product_id: p2.id, user_id:user5.id)
+Review.create(star_rating: 3, comment: 'Not bad', product_id: p1.id, user_id:user2.id)
+Review.create(star_rating: 5, comment: 'Good work', product_id: p3.id, user_id:user1.id)
+Review.create(star_rating: 5, comment: 'Amazing experience', product_id: p1.id, user_id:user3.id)
+Review.create(star_rating: 2, comment: 'Expected better', product_id: p3.id, user_id:user4.id)
+Review.create(star_rating: 1, comment: 'Just bad', product_id: p4.id, user_id:user3.id)
+Review.create(star_rating: 5, comment: 'Will come back', product_id: p2.id, user_id:user5.id)
+
+
+
+
+
+
+
 puts "Seeding done!"
